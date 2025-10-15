8d0fbfa88279ffdc222fa2c0d25313eccef7f7fd
BalkundeAkash <your_github_email@example.com>
2025-10-15 21:53:03 +0530

    15-Oct-2025  09:52PM - Implemented Logical Operators in Java

---
+++ Operators_01/src/Code_06_Logical_Operators.java
@@ -0,0 +1,16 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 9:28:40 pm
+ * @use Logical Operators @2025
+ */
+public class Code_06_Logical_Operators {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		System.out.println("a && b: " + (a && b)); // Logical AND
+		System.out.println("a || b: " + (a || b)); // Logical OR
+		System.out.println("!a : " + (!a)); // Logical NOT
+	}
+}
