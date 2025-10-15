97629654e13f84f2fccbc968cf26ce99d4338eb8
BalkundeAkash <your_github_email@example.com>
2025-10-15 22:00:40 +0530

    15-Oct-2025  10:00Pm - Implemented Ternary Operator in java

---
+++ Operators_01/src/Code_07_Ternary_Operator.java
@@ -0,0 +1,14 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 9:55:02 pm
+ * @use Ternary Operator @2025
+ */
+public class Code_07_Ternary_Operator {
+	public static void main(String[] args) {
+		int age = 18;
+		String result = (age >= 18) ? "Adult" : "Minor"; // Check if age >=18
+		System.out.println("Result: " + result); // print if adult or minor
+
+	}
+}
