d2a51decdace034ba2e2e789fb5e7c24d005331f
BalkundeAkash <your_github_email@example.com>
2025-10-15 21:08:26 +0530

    15-Oct-2025  09:08Pm - Implemented Bitwise operator in java

---
+++ Operators_01/src/Code_03_bitwise_operators.java
@@ -0,0 +1,18 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 9:03:41 pm
+ * @use bitwise_operators @2025
+ */
+public class Code_03_bitwise_operators {
+
+	public static void main(String[] args) {
+		int a = 5; // Binary : 0101
+		int b = 3; // Binary : 0011
+
+		System.out.println("a & b: " + (a & b)); // AND Operation
+		System.out.println("a | b:" + (a | b)); // OR Operation
+		System.out.println("a ^ b:" + (a ^ b)); // XOR Operation
+
+	}
+}
