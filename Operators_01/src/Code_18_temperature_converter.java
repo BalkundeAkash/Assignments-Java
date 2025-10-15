a671f9d58388318a61384a022315d5f7d4b28c9c
BalkundeAkash <your_github_email@example.com>
2025-10-15 23:26:33 +0530

    15-Oct-2025  11:26PM - Implemented Temperature converter in java

---
+++ Operators_01/src/Code_18_temperature_converter.java
@@ -0,0 +1,15 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 11:23:51 pm
+ * @use temperature_converter 
+ * @2025
+ */
+public class Code_18_temperature_converter {
+
+	public static void main(String[] args) {
+		double celsius = 25;
+		double fahrenheit = (celsius * 9 / 5) + 32; // Convert Celsius to Fahrenheit
+		System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+	}
+}
