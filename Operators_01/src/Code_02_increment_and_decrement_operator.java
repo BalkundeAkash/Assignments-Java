02640a3d995a908b8b1d128df82dfb1b8ce318b4
BalkundeAkash <your_github_email@example.com>
2025-10-15 21:00:30 +0530

    15-Oct-2025  08:59Pm - Implemented Increment & Decrement Operator in java

---
+++ Operators_01/src/Code_02_increment_and_decrement_operator.java
@@ -0,0 +1,20 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 8:53:43 pm
+ * @use increment_and_decrement_operator @2025
+ */
+
+public class Code_02_increment_and_decrement_operator {
+
+	public static void main(String[] args) {
+		int a = 10; // Initialize a with 10
+		System.out.println("Initial a: " + a); // Display initial value of a
+		System.out.println("Post-Increment: " + (a++)); // Post-Increment a , then Display it
+		System.out.println("Pre-Increment: " + (++a)); // Pre-Increment a , then Display it
+		System.out.println("Post-Decrement: " + (a--)); // Post-Decrement a , then Display it
+		System.out.println("pre-Increment: " + (--a)); // Pre-Decrement a , then display it
+
+	}
+
+}
