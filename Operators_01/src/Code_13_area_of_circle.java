7be8f940068053abc2619941ee976d2e56ed6ba8
BalkundeAkash <your_github_email@example.com>
2025-10-15 22:53:07 +0530

    15-Oct-2025  10:52PM - Implemented area_of_circle in java

---
+++ Operators_01/src/Code_13_area_of_circle.java
@@ -0,0 +1,16 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 10:49:28 pm
+ * @use area_of_circle 
+ * @2025
+ */
+public class Code_13_area_of_circle {
+
+	public static void main(String[] args) {
+		double radius = 7.5;
+		double area = Math.PI * radius * radius; // Area formula πr²
+		System.out.println("Area of Circle:" + area);
+
+	}
+}
