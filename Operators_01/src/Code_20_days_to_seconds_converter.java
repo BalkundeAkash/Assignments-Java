381343a07032dfcb17a811ee894d40e42d72979f
BalkundeAkash <your_github_email@example.com>
2025-10-15 23:39:17 +0530

    15-Oct-2025  11:38PM - Implemented Days to seconds to converter

---
+++ Operators_01/src/Code_20_days_to_seconds_converter.java
@@ -0,0 +1,14 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 11:33:04 pm
+ * @use days_to_seconds_converter 
+ * @2025
+ */
+public class Code_20_days_to_seconds_converter {
+	public static void main(String[] args) {
+		int days = 5;
+		int seconds = days * 24 * 60 * 60 ;//Convert days to seconds
+		System.out.println("Total Seconds: " + seconds);
+	}
+}
