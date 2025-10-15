9427006cec93c69468e8f3033ddfa77a96883d4e
BalkundeAkash <your_github_email@example.com>
2025-10-15 22:11:32 +0530

    15-Oct-2025  10:11Pm - Implemented Compound Assignment in java

---
+++ Operators_01/src/Code_08_Compound_Assignment.java
@@ -0,0 +1,18 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 10:02:06 pm
+ * @use Compound Assignment 
+ * @2025
+ */
+public class Code_08_Compound_Assignment {
+	public static void main(String[] args) {
+		int num = 10;
+		num += 5; // Equivalent to num = num + 5
+		System.out.println("After += : " + num);
+		num *= 2; // Equivalent to num = * 2
+		System.out.println("After *= : " + num);
+		num %= 3; // Equivalent to num = num % 3
+		System.out.println("After %= : " + num);
+	}
+}
