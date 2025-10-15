b028449d417ed029f0fa1f08b0917e0bd6c43e2b
BalkundeAkash <your_github_email@example.com>
2025-10-15 19:30:01 +0530

    15-Oct2025  07:29Pm - Implemented the Airthmetic Operators in java

---
+++ Operators_01/src/Code_01_Airthmetic_Operators.java
@@ -0,0 +1,14 @@
+
+class Airthemetic {
+
+	public static void main(String[] args) {
+		int a = 20, b = 20; // Declare two integers a & b with initial values
+
+		System.out.println("Addition :" + (a + b)); // Add a & b
+		System.out.println("Substraction :" + (a - b)); // Subtract a from b
+		System.out.println("Multiplication :" + (a * b)); // Multiply a and b
+		System.out.println("Division : " + (a / b)); // Divide a by b
+		System.out.println("Modulus : " + (a % b)); // Modulus operation gives the remainder of a /b
+
+	}
+}
