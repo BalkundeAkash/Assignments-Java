1f9f56076fd00f5a3a6d3a7ba1630d83145ad672
BalkundeAkash <your_github_email@example.com>
2025-10-15 22:24:52 +0530

    15-Oct-2025  10:24PM - Implemented the Distance Formula in java

---
+++ Operators_01/src/Code_10_Distance_Formula.java
@@ -0,0 +1,15 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 10:19:18 pm
+ * @use Distance Formula 
+ * @2025
+ */
+public class Code_10_Distance_Formula {
+
+	public static void main(String[] args) {
+		int x1 = 2, y1 = 3, x2 = 10, y2 = 8;
+		double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Distance: " + distance); // Print Calculated Distance
+	}
+}
