cb3b1897bd5d1a1a2f9ca0908e5c86c753f96b65
BalkundeAkash <your_github_email@example.com>
2025-10-15 21:25:44 +0530

    15-Oct-2025  09:24Pm - Implmented Relational Operators in java

---
+++ Operators_01/src/Code_05_RelationalOperators.java
@@ -0,0 +1,20 @@
+/**
+ * @author Akash D Balkunde
+ * @date 15-Oct-2025
+ * @time 9:16:13 pm
+ * @use RelationalOperators @2025
+ */
+public class Code_05_RelationalOperators {
+
+	public static void main(String[] args) {
+		int x = 10, y = 20;
+
+		System.out.println("x == y:" + (x == y)); // Checks if x is equals to y
+		System.out.println("x != y:" + (x != y)); // Check if x is not equal to y
+		System.out.println("x > y:" + (x > y)); // Check if x is greater than y
+		System.out.println("x < y:" + (x < y)); // Check if x is less than y
+		System.out.println("x >= y:" + (x >= y)); // Check if x is greater than or equal to y
+		System.out.println("x <= y:" + (x <= y)); // Check is x is less than or equal to y
+
+	}
+}
